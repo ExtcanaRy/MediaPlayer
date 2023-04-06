fc882cfd813b4e7cf2062ebe9a96ec03e41deef3
WillowSauceR <WillowSauceR@outlook.com>
2023-04-07 00:17:28 +0800

    feat: Add custom music playing function

--- include/mediaplayer/plugin.h
+++ include/mediaplayer/plugin.h
@@ -2,9 +2,12 @@
 #pragma comment(lib, "littlehooker.lib")
 #include <littlehooker/littlehooker.h>
 #include <time.h>
+
 #include "cpp_string.h"
 #include "logger.h"
 #include "music_player.h"
+#include "command.h"
+
 #include "mc/network.h"
 #include "mc/actor.h"
 #include "mc/level.h"
