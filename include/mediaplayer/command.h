adf1ae05750a2e305aeb14c93fced550319bc559
WillowSauceR <WillowSauceR@outlook.com>
2023-04-07 00:12:50 +0800

    feat: Add player command processor

---
+++ include/mediaplayer/command.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "music_player.h"
+#include "file_utils.h"
+#include "mc/actor.h"
+#include "mc/network.h"
+#include "mc/level.h"
+
+bool process_cmd(struct player *player, const char *cmd);
