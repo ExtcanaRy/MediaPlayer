e38bde780780473fae9d898764f820036af2cb75
WillowSauceR <WillowSauceR@outlook.com>
2023-04-09 22:32:02 +0800

    refactor: using inline func at LLPreLoaderAPI
    hook_func_ll
    dlsym_ll
    hook_func_auto
    dlsym_auto

--- include/mediaplayer/LLPreLoaderAPI.h
+++ include/mediaplayer/LLPreLoaderAPI.h
@@ -1,14 +1,48 @@
 #pragma once
 #include <windows.h>
 #include <psapi.h>
-#include <littlehooker/littlehooker.h>
+#include <stdbool.h>
+
+bool hook_func(void *hook_func, void *detour_func, void *original_func);
+void *dlsym(const char *sym);
 
 uintptr_t FindSignatureModule(const char* szModule, const char* szSignature);
 
-bool hook_func_ll(void *oldFunc, void **pOutOld, void *newFunc);
-bool hook_func_auto(void *target_func, void *detour_func, void *original_func);
-
-void *dlsym_ll(const char *x);
-void *dlsym_auto(const char *sym);
-
 extern bool using_ll_preloader_api;
+
+inline bool hook_func_ll(void *oldFunc, void **pOutOld, void *newFunc)
+{
+	static uintptr_t func = 0;
+	if (!func)
+		func = FindSignatureModule("LLPreloader.dll",
+			"40 53 56 57 48 83 EC 40  48 8B 05 ?? ?? 03 00 48");
+	((int (*)(void *oldFunc, void **pOutOld, void *newFunc)) ((void *)func)) (oldFunc, pOutOld, newFunc);
+
+	return true;
+}
+
+inline bool hook_func_auto(void *target_func, void *detour_func, void *original_func)
+{
+	if (using_ll_preloader_api)
+		return hook_func_ll(target_func, original_func, detour_func);
+	else
+		return hook_func(target_func, detour_func, original_func);
+}
+
+inline void *dlsym_ll(const char *x)
+{
+	static uintptr_t func = 0;
+	if (!func)
+		func = FindSignatureModule("LLPreloader.dll",
+			"40 55 53 56 48 8D 6C 24  B9 48 81 EC 90 00 00 00");
+	
+	return ((void *(*)(const char *x)) ((void *)func)) (x);
+}
+
+inline void *dlsym_auto(const char *sym)
+{
+	if (using_ll_preloader_api)
+		return dlsym_ll(sym);
+	else
+		return dlsym(sym);
+}
--- src/FindSignatureModule.c
+++ src/FindSignatureModule.c
@@ -6,7 +6,6 @@
 // which is licensed under GPL-3.0.
 // Modified by WillowSauceR in order to support use in C code.
 
-// START
 #define INRANGE(x, a, b) (x >= a && x <= b)
 #define GET_BYTE(x) (GET_BITS(x[0]) << 4 | GET_BITS(x[1]))
 #define GET_BITS(x) (INRANGE((x & (~0x20)), 'A', 'F') ? ((x & (~0x20)) - 'A' + 0xa) : (INRANGE(x, '0', '9') ? x - '0' : 0))
@@ -60,47 +59,4 @@ uintptr_t FindSignatureModule(const char* szModule, const char* szSignature) {
 		}
 	}
 	return 0u;
-}
-// END
-
-bool hook_func_ll(void *oldFunc, void **pOutOld, void *newFunc)
-{
-	static uintptr_t func = 0;
-	if (!func)
-		func = FindSignatureModule("LLPreloader.dll",
-			"40 53 56 57 48 83 EC 40  48 8B 05 ?? ?? 03 00 48");
-	VIRTUAL_CALL(func,
-		int (*)(void *oldFunc, void **pOutOld, void *newFunc),
-		oldFunc, pOutOld, newFunc);
-
-	return true;
-}
-
-bool hook_func_auto(void *target_func, void *detour_func, void *original_func)
-{
-	if (using_ll_preloader_api)
-		return hook_func_ll(target_func, original_func, detour_func);
-	else
-		return hook_func(target_func, detour_func, original_func);
-}
-
-void *dlsym_ll(const char *x)
-{
-	static uintptr_t func = 0;
-	if (!func)
-		func = FindSignatureModule("LLPreloader.dll",
-			"40 55 53 56 48 8D 6C 24  B9 48 81 EC 90 00 00 00");
-	
-	return 
-		VIRTUAL_CALL(func,
-			void *(*)(const char *x),
-			x);
-}
-
-void *dlsym_auto(const char *sym)
-{
-	if (using_ll_preloader_api)
-		return dlsym_ll(sym);
-	else
-		return dlsym(sym);
 }
