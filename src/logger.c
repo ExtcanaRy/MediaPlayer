2d55bae4bb74de1aae9ea79ac182fc3aa8a625e0
WillowSauceR <WillowSauceR@outlook.com>
2023-04-10 04:24:23 +0800

    refactor: 0x0d00
    0x0721
    私のオナニーを見てください！

--- src/logger.c
+++ src/logger.c
@@ -4,5 +4,5 @@ void server_logger(const char *msg, enum log_level level)
 {
 	TLCALL("?log@BedrockLog@@YAXW4LogCategory@1@V?$bitset@$02@std@@W4LogRule@1@W4LogAreaID@@IPEBDH4ZZ",
 		char (*)(unsigned int a1, char a2, int a3, int a4, unsigned int a5, const char *a6, int a7, const char *a8),
-		0, 1, 0, 12, level, "HOOKER->LOG", 114514, msg);
+		0, 1, 0x0d00, 0, level, "私のオナニーを見てください！", 0x0721, msg);
 }
