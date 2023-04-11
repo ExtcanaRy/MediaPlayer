d4880cd7ead71bf081b9983e036a0381170b1493
WillowSauceR <WillowSauceR@outlook.com>
2023-04-12 01:04:59 +0800

    feat: Add func & global variables to file_utils
    char bds_path[260];
    char data_path[260];
    char data_path_nbs[260];
    char data_path_video[260];
    
    char **get_foldernames(const char *directory, int *count);
    void free_foldernames(char **foldernames, int count);

--- include/mediaplayer/file_utils.h
+++ include/mediaplayer/file_utils.h
@@ -3,5 +3,12 @@
 #include <locale.h>
 #include <dirent/dirent.h>
 
+extern char bds_path[];
+extern char data_path[];
+extern char data_path_nbs[];
+extern char data_path_video[];
+
 char **get_filenames(const char *directory, int *count);
+char **get_foldernames(const char *directory, int *count);
 void free_filenames(char **filenames, int count);
+void free_foldernames(char **filenames, int count);
--- src/file_utils.c
+++ src/file_utils.c
@@ -1,5 +1,10 @@
 #include <mediaplayer/file_utils.h>
 
+char bds_path[260];
+char data_path[260];
+char data_path_nbs[260];
+char data_path_video[260];
+
 char **get_filenames(const char *directory, int *count)
 {
 	setlocale(LC_ALL, "");
@@ -16,7 +21,7 @@ char **get_filenames(const char *directory, int *count)
 	}
 
 	while ((ent = readdir(dir)) != NULL) {
-		if (ent->d_type == DT_REG && ent->d_name[0] != '.' && strstr(ent->d_name, ".nbs")) {
+		if (ent->d_type == DT_REG && ent->d_name[0] != '.') {
 			char *filename = malloc(ent->d_namlen + 1);
 			strcpy(filename, ent->d_name);
 
@@ -32,10 +37,50 @@ char **get_filenames(const char *directory, int *count)
 	return filenames;
 }
 
+char **get_foldernames(const char *directory, int *count)
+{
+	setlocale(LC_ALL, "");
+	DIR *dir;
+	struct dirent *ent;
+	int i = 0;
+	char **foldernames = NULL;
+
+	dir = opendir(directory);
+	if (dir == NULL) {
+		CreateDirectoryA(directory, NULL);
+		*count = 0;
+		return foldernames;
+	}
+
+	while ((ent = readdir(dir)) != NULL) {
+		if (ent->d_type == DT_DIR && ent->d_name[0] != '.') {
+			char *foldername = malloc(ent->d_namlen + 1);
+			strcpy(foldername, ent->d_name);
+
+			foldernames = realloc(foldernames, sizeof(char *) * (i + 1));
+			foldernames[i] = foldername;
+			i++;
+		}
+	}
+
+	closedir(dir);
+
+	*count = i;
+	return foldernames;
+}
+
 void free_filenames(char **filenames, int count)
 {
 	for (int i = 0; i < count; i++) {
 		free(filenames[i]);
 	}
 	free(filenames);
+}
+
+void free_foldernames(char **foldernames, int count)
+{
+	for (int i = 0; i < count; i++) {
+		free(foldernames[i]);
+	}
+	free(foldernames);
 }
