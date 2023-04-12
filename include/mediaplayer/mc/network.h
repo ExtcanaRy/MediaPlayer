645dc0078353f41ceb969313a3dad32b834d4ca9
WillowSauceR <WillowSauceR@outlook.com>
2023-04-12 23:32:39 +0800

    feat: Add func send_boss_event_packet

--- include/mediaplayer/mc/network.h
+++ include/mediaplayer/mc/network.h
@@ -4,6 +4,12 @@
 #include "position.h"
 #include "actor.h"
 
+enum boss_bar_event_type {
+	BOSS_BAR_DISPLAY,
+	BOSS_BAR_UPDATE,
+	BOSS_BAR_HIDE,
+};
+
 struct server_network_handler;
 
 inline uintptr_t create_packet(int type);
@@ -12,3 +18,5 @@ inline void send_network_packet(struct player *player, uintptr_t pkt);
 void send_play_sound_packet(struct player *player, const char *sound_name,
 			 				struct vec3 *pos, float volume, float pitch);
 void send_text_packet(struct player *player, int mode, const char *msg);
+void send_boss_event_packet(struct player *player, const char *name, 
+							float per, enum boss_bar_event_type type);
--- src/mc/network.c
+++ src/mc/network.c
@@ -55,3 +55,17 @@ void send_text_packet(struct player *player, int mode, const char *msg)
 	free(player_name_cpp_str);
 	free(msg_cpp_str);
 }
+
+void send_boss_event_packet(struct player *player, const char *name,
+							float per, enum boss_bar_event_type type)
+{
+	uintptr_t pkt = create_packet(74);
+	uintptr_t unique_id = DEREFERENCE(uintptr_t, get_unique_id((struct actor *)player), 0);
+	struct string *name_cpp_str = string.string(name);
+	DEREFERENCE(uintptr_t, pkt, 56) = unique_id;
+	DEREFERENCE(int, pkt, 72) = type;
+ 	memcpy((void *)(pkt + 80), name_cpp_str, 32);
+	DEREFERENCE(float, pkt, 112) = per;
+	send_network_packet(player, pkt);
+	free(name_cpp_str);
+}
