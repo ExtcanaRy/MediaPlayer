81c4d010e1476b5f69439c2ad56e1749ae0925eb
Syncyn-R <xiziya_r@outlook.com>
2024-02-02 06:16:46 +0800

    fix: fix server_logger no va_end

--- src/logger.c
+++ src/logger.c
@@ -7,4 +7,5 @@ void server_logger(enum log_level level, const char *msg, ...)
 	SYMCALL(S_BedrockLog__log_va,
 		char (*)(unsigned int category, char set, int rule, int area, unsigned int level, const char *tag, int tid, const char *format, va_list args),
 		0, 1, 0x0d00, 0, level, "私のオナニーを見てください！", 0x0721, msg, va);
+	va_end(va);
 }
